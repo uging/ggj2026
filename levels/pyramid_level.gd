e1606938494771c400c05afea85fe6dde7289c94
uging <jesison@outlook.com>
2026-02-01 23:39:37 +0800

    created hud global

--- levels/pyramid_level.gd
+++ levels/pyramid_level.gd
@@ -1,13 +1,14 @@
 extends Node2D
-var player: Node2D = null
 
+
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("The Tower!")
+	print("The Pyramid!")
 	#await get_tree().root.child_entered_tree  # Wait tree ready
 	
 	add_child(Global.player)
 	Global.player.is_top_down = false
-	Global.player.global_position = Vector2.ZERO
+	Global.player.global_position = Vector2(600, 400) 
 	
 	add_child(Global.hud)
 
--- main/pyramid_area.gd
+++ main/pyramid_area.gd
@@ -22,4 +22,12 @@ func _on_body_exited(body):
 
 func _input(event):
 	if event.is_action_pressed("ui_accept") and player_in_range:
-		get_tree().change_scene_to_file(next_scene_path)
+		# 1. Detach player from old scene FIRST
+		if Global.player and Global.player.get_parent():
+			Global.player.get_parent().remove_child(Global.player)
+			
+		if Global.hud and Global.hud.get_parent():
+			Global.hud.get_parent().remove_child(Global.hud)
+		
+		# 2. Safe scene change
+		get_tree().call_deferred("change_scene_to_file", next_scene_path)
